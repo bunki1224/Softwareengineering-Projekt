7019cd54c1c7ceb15ff450800c23cbb9bc2daad2
Kartoffeleintopf <cedric-noe@mail.de>
2024-11-07 12:18:44 +0100

    Delete TripAhead/src/components/Activity.jsx
    removed Activity component

--- TripAhead/src/components/Activity.jsx
+++
@@ -1,7 +0,0 @@
-export default function Activity(){
-    return (
-        <div style={{ borderRadius: '5px', backgroundColor: '#78A5A7'}}>
-        <Rating defaultValue={2.5} precision={0.5}/>
-        </div>
-    );
-}

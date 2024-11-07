6ef5bc95d9fb1dceb7350aee5d2d0bb715c1fd75
z004u4mb <cedric-arno-aldo.noeldechen@siemens-energy.com>
2024-11-07 12:00:40 +0100

    added Activity component

---
+++ TripAhead/src/components/Activity.jsx
@@ -0,0 +1,7 @@
+export default function Activity(){
+    return (
+        <div style={{ borderRadius: '5px', backgroundColor: '#78A5A7'}}>
+        <Rating defaultValue={2.5} precision={0.5}/>
+        </div>
+    );
+}

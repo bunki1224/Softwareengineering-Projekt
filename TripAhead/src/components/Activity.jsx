5a858b988a90ff7e3a0c2d0f59432fefe944fe4a
Kartoffeleintopf <cedric-noe@mail.de>
2024-11-10 17:17:01 +0100

    resized div of Activity
    yeet

--- TripAhead/src/components/Activity.jsx
+++ TripAhead/src/components/Activity.jsx
@@ -3,7 +3,7 @@ import CustomRating from "./CustomRating";
 export default function Activity(){
     return (
         <>
-        <div style={{ borderRadius: '5px', backgroundColor: '#78A5A7'}}>
+        <div style={{ borderRadius: '5px', backgroundColor: '#78A5A7', width: '450px', height: '150px'}}>
         <CustomRating/>
         </div>
         </>

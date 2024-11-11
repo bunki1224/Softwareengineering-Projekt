97103fd528a733322c70690e10f80a6f71ce54a2
bunki1224 <j.bassing04@gmail.com>
2024-11-11 13:42:59 +0100

    fixed format in App.jsx

--- TripAhead/src/App.jsx
+++ TripAhead/src/App.jsx
@@ -6,35 +6,32 @@ import CustomRating from "./components/CustomRating";
 function App() {
 
   return (
-	<>
-<Typography variant="h1">
-TripAhead
-</Typography>
+    <>
+      <Typography variant="h1">
+      TripAhead
+      </Typography>
 
-<Button variant="contained">New Activity +</Button>
+      <Button variant="contained">New Activity +</Button>
 
-<Activity
-  title="Tokyo Skytree"
-  address="1 Chome-1-2 Oshiage, Sumida City, Tokyo"
-  price={20}
-  tags={['Sightseeing']}
-  rating={5}
-  image="./bilder/Skytree.png"
-/>
+      <Activity
+        title="Tokyo Skytree"
+        address="1 Chome-1-2 Oshiage, Sumida City, Tokyo"
+        price={20}
+        tags={['Sightseeing']}
+        rating={5}
+        image="./bilder/Skytree.png"
+      />
 
-<Activity
-  title="Meiji-Shrine"
-  address="1-1 Yoyogikamizonochō, Shibuya City, Tokyo"
-  price={0}
-  tags={['Free', 'Temple', 'test']}
-  rating={4.5}
-  image="./bilder/Meiji.jpg"
-/>
-
-
-
-  </>
-)
+      <Activity
+        title="Meiji-Shrine"
+        address="1-1 Yoyogikamizonochō, Shibuya City, Tokyo"
+        price={0}
+        tags={['Free', 'Temple', 'test']}
+        rating={4.5}
+        image="./bilder/Meiji.jpg"
+      />
+    </>
+  )
 }
 
 export default App;

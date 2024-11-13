2f6a4545f6e7d016b48f476796887f3fbdf6a31c
bunki1224 <j.bassing04@gmail.com>
2024-11-13 10:37:14 +0100

    prepared live Demo

--- TripAhead/src/App.jsx
+++ TripAhead/src/App.jsx
@@ -1,13 +1,14 @@
 import { Button, Typography } from "@mui/material";
 import Activity from "./components/Activity";
 import CustomRating from "./components/CustomRating";
+import TextField from "@mui/material/TextField";
 
 
 function App() {
 
   return (
     <>
-      <Typography variant="h1">
+      {/* <Typography variant="h1">
       TripAhead
       </Typography>
 
@@ -29,7 +30,149 @@ function App() {
         tags={['Free', 'Temple', 'test']}
         rating={4.5}
         image="./bilder/Meiji.jpg"
+      /> */}
+
+<div
+  id="wrapper"
+  style={{
+    width: '95vw',
+    height: '80vh',
+    backgroundColor: '#008888',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+  }}
+>
+  <div
+    style={{
+      display: 'flex',
+      justifyContent: 'space-around',
+      alignItems: 'center',
+      width: '100%',
+      height: '100%',
+    }}
+  >
+    <div
+      id="backlog"
+      style={{
+        backgroundColor: '#424549',
+        width: '45%',
+        height: '80%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'start',
+        margin: '1rem',
+        borderRadius: '10px',
+        overflowY: 'auto', // Ermöglicht vertikales Scrollen
+        overflowX: 'hidden', // Verhindert horizontales Scrollen
+        padding: '1rem',
+      }}
+    >
+      <h1
+        style={{
+          fontSize: '2rem',
+          fontWeight: 'bold',
+          textAlign: 'center',
+          marginBottom: '1rem',
+        }}
+      >
+        Backlog
+      </h1>
+      <TextField
+        label="suche Orte / Hotels"
+        sx={{
+          width: '100%',
+          backgroundColor: 'white',
+          borderRadius: '20px',
+          '& .MuiOutlinedInput-root': {
+            borderRadius: '20px',
+          },
+          '& .MuiOutlinedInput-notchedOutline': {
+            borderRadius: '20px',
+          },
+        }}
+      ></TextField>
+        <Activity
+        title="Meiji-Shrine"
+        address="1-1 Yoyogikamizonochō, Shibuya City, Tokyo"
+        price={0}
+        tags={['Free', 'Temple', 'test']}
+        rating={4.5}
+        image="./bilder/Meiji.jpg"
+      /> 
+      
+    </div>
+    
+    <div
+      id="timeline"
+      style={{
+        backgroundColor: '#424549',
+        width: '45%',
+        height: '80%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'start',
+        margin: '1rem',
+        borderRadius: '10px',
+        overflowY: 'auto', // Ermöglicht vertikales Scrollen
+        overflowX: 'hidden', // Verhindert horizontales Scrollen
+        padding: '1rem',
+      }}
+    >
+      <h1
+        style={{
+          fontSize: '2rem',
+          fontWeight: 'bold',
+          textAlign: 'center',
+          marginBottom: '1rem',
+        }}
+      >
+        Timeline
+      </h1>
+      <Activity
+      style={{ width: '100%' }}
+        title="Meiji-Shrine"
+        address="1-1 Yoyogikamizonochō, Shibuya City, Tokyo"
+        price={0}
+        tags={['Free', 'Temple', 'test']}
+        rating={4.5}
+        image="./bilder/Meiji.jpg"
       />
+      <Activity
+        title="Meiji-Shrine"
+        address="1-1 Yoyogikamizonochō, Shibuya City, Tokyo"
+        price={0}
+        tags={['Free', 'Temple', 'test']}
+        rating={4.5}
+        image="./bilder/Meiji.jpg"
+      />
+      <Activity
+        title="Meiji-Shrine"
+        address="1-1 Yoyogikamizonochō, Shibuya City, Tokyo"
+        price={0}
+        tags={['Free', 'Temple', 'test']}
+        rating={4.5}
+        image="./bilder/Meiji.jpg"
+      />
+      <Activity
+        title="Meiji-Shrine"
+        address="1-1 Yoyogikamizonochō, Shibuya City, Tokyo"
+        price={0}
+        tags={['Free', 'Temple', 'test']}
+        rating={4.5}
+        image="./bilder/Meiji.jpg"
+      />
+      {/* Weitere Inhalte hier */}
+    </div>
+  </div>
+</div>
     </>
   )
 }
